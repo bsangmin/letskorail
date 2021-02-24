9eb2725d2a7bc2ff65a9a95fbc1179332a9731a5
bsangmin <garlicpy@gmail.com>
2021-02-25 01:43:57 +0900

    change sample code

--- sample.py
+++ sample.py
@@ -57,11 +57,11 @@ rsv = korail.reserve(trains[0], seat_opt=SeatOption.SPECIAL_FIRST)
 ###########################
 # 선호 좌석 예약
 ###########################
-seat = (
-    trains[0].cars[3].select_seat(location="출입문", position="내측"),
+seats = (
+    trains[0].cars[3].select_seats(location="출입문", position="내측"),
 )  # 3호차 좌석 예약
 
-rsv = korail.reserve(trains[0], seat_opt=[seat])  # Iterable 형태로 넘겨줘야 함
+rsv = korail.reserve(trains[0], seat_opt=seats)  # Iterable 형태로 넘겨줘야 함
 print(rsv.info)
 
 ###########################
